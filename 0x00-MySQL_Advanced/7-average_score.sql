7b8210b73d738ade9b2f54a7ad248d0086bfbb6e
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-04-25 13:48:29 +0000

    fixing task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,5 +1,5 @@
 -- Write a SQL script that creates a stored procedure ComputeAverageScoreForUser
--- that computes and store the average score for a student. Note: An average score can be a decimal
+-- that computes and store the average score for a student. !!: An average score can be a decimal
 
 -- Procedure ComputeAverageScoreForUser is taking 1 input:
 -- 		user_id, a users.id value (you can assume user_id is linked to an existing users)
