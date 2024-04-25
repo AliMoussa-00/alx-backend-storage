256b9b3a474143c0001b113c02d608f7280a54fc
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-04-25 13:52:15 +0000

    this is driving me crazy

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,17 +4,20 @@
 -- Procedure ComputeAverageScoreForUser is taking 1 input:
 -- 		user_id, a users.id value (you can assume user_id is linked to an existing users)
 
-DELIMITER $$
-
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+DELIMITER //
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
 BEGIN
-DECLARE average FLOAT DEFAULT 0;
-SELECT SUM(score) / COUNT(*) INTO average FROM corrections
-WHERE user_id = user_id;
 
--- updating the users score
-UPDATE users SET average_score = average WHERE id = user_id;
+DECLARE user_score DECIMAL(10, 2);
 
-END $$
+-- SELECT AVG(score) INTO user_score FROM corrections WHERE user_id = user_id;
+SELECT SUM(score) / COUNT(*) INTO user_score FROM corrections WHERE corrections.user_id = user_id;
+
+UPDATE users SET average_score = user_score WHERE id = user_id;
+
+END//
 
 DELIMITER ;
